73403a54446bfcdce5da2ee8d79c5bf72d89c275
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2017-07-20 17:43:47 +0200

    Remove public from tableview datasource funcs

--- TestTableViewHeader/ViewController.swift
+++ TestTableViewHeader/ViewController.swift
@@ -27,11 +27,11 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
 
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 50
     }
 
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
         cell.label.text = "\(indexPath.row)"
         return cell
